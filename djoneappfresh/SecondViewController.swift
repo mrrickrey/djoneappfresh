b23dee8bbb0c31d074b306014defabf24fd6e92a
mrrickrey <mrrickrey@gmail.com>
2016-09-24 11:47:20 -0400

    Initial Commit

---
+++ djoneappfresh/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  djoneappfresh
+//
+//  Created by Enrique Reyes on 9/24/16.
+//  Copyright © 2016 Enrique Reyes. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
