1410c77ad087fa2462093f814210f7d2401ac11d
mrrickrey <mrrickrey@gmail.com>
2016-09-24 20:33:03 -0400

    update1

--- djoneappfresh/FirstViewController.swift
+++ djoneappfresh/FirstViewController.swift
@@ -15,11 +15,58 @@ class FirstViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func facebookPressed(_ sender: AnyObject) {
+        
+        let fbURL: NSURL = NSURL(string: "http://m.facebook.com/djoneoficial/")!
+        let fbURLID: NSURL = NSURL(string: "fb://profile/110641415698826")!
+        //Opening app or safari depending on user interaction
+        if (UIApplication.shared.canOpenURL(fbURLID as URL)) {
+            UIApplication.shared.openURL(fbURLID as URL)
+        } else {
+            UIApplication.shared.openURL(fbURL as URL)
+        }
+
+    }
+   
+    @IBAction func snapPressed(_ sender: UIButton) {
+        
+        let snapURL: NSURL = NSURL(string: "http://snapchat.com/add/soydjone")!
+        let snapURLID: NSURL = NSURL(string: "snapchat://add/soydjone")!
+        
+        if (UIApplication.shared.canOpenURL(snapURLID as URL)) {
+            UIApplication.shared.openURL(snapURLID as URL)
+        } else {
+            UIApplication.shared.openURL(snapURL as URL)
+        }
+
     }
 
+    @IBAction func instaPressed(_ sender: UIButton) {
+        
+        let instaURL: NSURL = NSURL(string: "https://www.instagram.com/soydjone/")!
+        let instaURLID: NSURL = NSURL(string: "instagram://user?username=soydjone")!
+        
+        if (UIApplication.shared.canOpenURL(instaURLID as URL)) {
+            UIApplication.shared.openURL(instaURLID as URL)
+        } else {
+            UIApplication.shared.openURL(instaURL as URL)
+        }
+        
+    }
+    
+    @IBAction func whatApPressed(_ sender: UIButton) {
+        
+        let snapURL: NSURL = NSURL(string: "http://snapchat.com/add/soydjone")!
+        let snapURLID: NSURL = NSURL(string: "snapchat://add/soydjone")!
+        
+        if (UIApplication.shared.canOpenURL(snapURLID as URL)) {
+            UIApplication.shared.openURL(snapURLID as URL)
+        } else {
+            UIApplication.shared.openURL(snapURL as URL)
+        }
 
+    }
+    
+    
 }
 
